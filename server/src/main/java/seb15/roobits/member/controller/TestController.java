720cfb4392acc1aae57d3cde8802d2c214b22e8d
CHOHYUNHWA <104138036+CHOHYUNHWA@users.noreply.github.com>
2022-09-19 14:24:40 +0900

    Feat/be/member (#64)
    * member frame create
    
    * feat:memberService logic create
    
    * feat: controller create
    
    * feat : Basic security create
    
    * fix: member Entity validation추가 정규표현식 미작성
    
    * fix member validation 적용
    
    * feat/fix : feat : 나의룸보기 구현, fix: deleteMember 기능 수정
    
    * feat: member 로그아웃 기능구현
    
    * feat : patchMember 기능 구현 외 회원가입,로그인,로그아웃,회원수정,회원삭제 테스트 완료
    
    * feat : 아이디찾기 기능구현
    
    * test: yml파일 삭제
    
    * test : gitignore 적용
    
    * feat: 나의룸 보기 기능
    
    * feat : 비밀번호 찾기기능 구현(이메일로 임시비밀번호발급)
    
    * fix :충돌 해결 2

---
+++ server/src/main/java/seb15/roobits/member/controller/TestController.java
@@ -0,0 +1,36 @@
+package seb15.roobits.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import seb15.roobits.member.dto.MemberDto;
+import seb15.roobits.member.entity.Member;
+import seb15.roobits.member.mapper.MemberMapper;
+import seb15.roobits.member.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class TestController {
+
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+
+    @GetMapping("/login")
+    public String login(){
+        return "loginForm";
+    }
+
+    @GetMapping("/join")
+    public String joinForm() {
+        return "joinForm";
+    }
+
+//    @PostMapping("/join")
+//    public String join(MemberDto.Join memberJoinDto) {
+//        Member member = memberMapper.joinToMember(memberJoinDto);
+//        Member createdMember = memberService.createMember(member);
+//        MemberDto.Response response = memberMapper.memberToResponse(createdMember);
+//        return "redirect:/home";
+//    }
+}

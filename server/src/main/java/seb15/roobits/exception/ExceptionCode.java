a5ed6b2724de36aa77d7ccbea97b87ae849a5206
EstelleYU <iamestelleyu@gmail.com>
2022-09-29 15:09:53 +0900

    0929 YU #2 - exceptionCode 정리

--- server/src/main/java/seb15/roobits/exception/ExceptionCode.java
+++ server/src/main/java/seb15/roobits/exception/ExceptionCode.java
@@ -3,31 +3,21 @@ package seb15.roobits.exception;
 import lombok.Getter;
 
 public enum ExceptionCode {
-
-    DDAY_NOT_VALID(400, "D-day has to be within 30 days from today"),
-    MEMBER_NOT_FOUND(404, "Member not found"),
     USERNAME_EXISTS(409, "Username already exists"),
-//    MEMBER_EXISTS(409, "Member exists"),
-    EMAIL_EXISTS(409,"Email already exists"),
-    INVALID_TOKEN(500 , "Invalid token"),
-
     INVALID_USERNAME_OR_PASSWORD(422 , "Invalid username or password"),
-    ROOBIT_NOT_FOUND(404, "Roobit not found"),
-
-
-
-    ROOBIT_CODE_EXISTS(409, "Roobit Code exists"),
-
-    ROOM_NOT_FOUND(404, "Room not found"),
-
-    NOT_IMPLEMENTATION(501, "Not Implementation"),
-
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EXISTS(409, "Member exists"),
     INVALID_MEMBER_STATUS(400, "Invalid member status"),
-
+    CANNOT_CHANGE_ROOM(403, "Room can not change"),
+    ROOM_NOT_FOUND(404, "Room not found"),
     ROOMNAME_ALREADY_EXISTS(409, "Room already exists"),
 
-    CANNOT_CHANGE_ROOM(403, "Room can not be changed more than twice");
-
+    ROOBIT_CODE_EXISTS(409, "Roobit Code exists"),
+    ROOBIT_NOT_FOUND(404, "Roobit not found"),
+    DDAY_NOT_VALID(400, "D-day has to be within 30 days from today"),
+    NOT_IMPLEMENTATION(501, "Not Implementation"),
+    EMAIL_EXISTS(409,"Email already exists"),
+    INVALID_TOKEN(500 , "Invalid token");
     @Getter
     private int status;
 

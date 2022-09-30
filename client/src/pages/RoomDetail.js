370dd7a22411dfc26f2ca60dee507ea8058bac48
nuuco <nuuco.coding@gmail.com>
2022-10-01 04:36:09 +0900

    feat: 룸 조회 데이터 받아오는 쿼리 작성

--- client/src/pages/RoomDetail.js
+++ client/src/pages/RoomDetail.js
@@ -1,14 +1,46 @@
 import Building from '../components/Building';
 import CreateRoobitBtn from '../components/CreateRoobitBtn';
-// import { roomDetailData } from '../data/DummyData';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { roomDetailData } from '../data/DummyData';
 
 const RoomDetail = () => {
+  const { roomId = 1 } = useParams();
+  //useLocation 으로 경로에서 roomId 받아오기
   //쿼리로 룸 정보 받아와서 저장
+  roomId;
+  const { data, isLoading } = useQuery(
+    'roobits',
+    () =>
+      //`${process.env.REACT_APP_API_URL}/rooms/${roomId}`
+      axios
+        .get(`/fakeurl`)
+        .then((res) => res.data)
+        .catch(() => roomDetailData),
+    { staleTime: 1000 * 60 * 5, retry: 0 }
+  );
+
+  let roomStatus, roomData, roobits;
+  if (!isLoading) {
+    ({ roomStatus, roomData, roobits } = data);
+  } else {
+    return <p>로딩 컴포넌트</p>;
+  }
+
   return (
     <div>
-      <h1>룸 이름</h1>
-      <Building />
-      <CreateRoobitBtn />
+      {roomStatus === 'closed' || roobits === undefined ? (
+        <p>룸 종료 페이지 컴포넌트</p>
+      ) : (
+        <>
+          <h1>{roomData.roomName}</h1>
+          <p>{roomData.restDay}</p>
+          <p>{roomData.dDay}</p>
+          <Building roobits={roobits} />
+          <CreateRoobitBtn />
+        </>
+      )}
     </div>
   );
 };

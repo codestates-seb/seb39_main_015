033b5f7ab73a69ee79041a457b8f2ed6c895c8fc
nuuco <nuuco.coding@gmail.com>
2022-10-01 04:38:50 +0900

    feat: Roobits 컴포넌트 생성

---
+++ client/src/components/Roobits.js
@@ -0,0 +1,12 @@
+//import catsImg from '../images/roomImg/03_cats_1x_w3200.png';
+//import lettersImg from '../images/roomImg/04_letters_1x_w400.png';
+
+const Roobits = ({ unitRoobits }) => {
+  return (
+    <div>
+      <p>{unitRoobits[0].nickname}</p>
+    </div>
+  );
+};
+
+export default Roobits;

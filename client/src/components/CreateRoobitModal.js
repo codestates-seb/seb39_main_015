4310e58ab3a4c9447a4a81de639e80f7eaf4498a
nuuco <nuuco.coding@gmail.com>
2022-10-01 14:02:09 +0900

    feat: 루빗 생성 모달 진행

--- client/src/components/CreateRoobitModal.js
+++ client/src/components/CreateRoobitModal.js
@@ -1,8 +1,33 @@
 import { FormWrapper } from '../styled/Style';
-//import Carousel from './Carousel';
+import Carousel from './Carousel';
 import { ReactComponent as CancelIcon } from '../images/cancel-icon.svg';
+import { useMemo } from 'react';
 
 const CreateRoobitModal = ({ handleOpenModal }) => {
+  const roobitTypes = useMemo(() => {
+    return [
+      {
+        type: 'roobit',
+        number: 1,
+      },
+      {
+        type: 'roobit',
+        number: 2,
+      },
+      {
+        type: 'roobit',
+        number: 2,
+      },
+      {
+        type: 'roobit',
+        number: 2,
+      },
+      {
+        type: 'roobit',
+        number: 2,
+      },
+    ];
+  }, []);
   return (
     <FormWrapper
       width="476px"
@@ -11,50 +36,15 @@ const CreateRoobitModal = ({ handleOpenModal }) => {
     >
       <CancelIcon stroke="#aaa" onClick={handleOpenModal} />
       <h2>Create a Roobits</h2>
-      <form></form>
-      {/* <form onReset={handleOnReset}>
-       <section>
-          <label htmlFor="room-name">룸 이름</label>
-          <input
-            id="room-name"
-            type="text"
-            placeholder="최대 20자까지 작성 가능합니다."
-            minLength={2}
-            maxLength={20}
-            name="roomName"
-            required
-            onChange={(e) => setRoomName(e.target.value.trim())}
-          />
-          <p>{roomNameMsg}</p>
+      <form>
+        <section>
+          <p>고양이 선택</p>
+          <Carousel cards={roobitTypes} />
         </section>
         <section>
-          <label htmlFor="d-day">D-day</label>
-          <DatePickerComponent dDayDate={dDayDate} setDdayDate={setDdayDate} />
+          <p>글 작성하기</p>
         </section>
-        <section>
-          <label htmlFor="max-roobits">최대 루빗 개수</label>
-          <select
-            id="roobits-num"
-            name="roobitAmount"
-            onChange={(e) => setRoobitAmount(Number(e.target.value))}
-          >
-            <option value="300">300 (max)</option>
-            <option value="250">250</option>
-            <option value="200">200</option>
-            <option value="150">150</option>
-            <option value="100">100</option>
-            <option value="50">50</option>
-          </select>
-        </section>
-        <section id="theme">
-          <label htmlFor="theme">테마 선택</label>
-          <Carousel cards={themes} setRoomTheme={setRoomTheme} />
-        </section>
-        <section>
-          <button type="reset">초기화</button>
-          <button onClick={handleOnSubmit}>룸 만들기</button>
-        </section> 
-      </form>*/}
+      </form>
     </FormWrapper>
   );
 };

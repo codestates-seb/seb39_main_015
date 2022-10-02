c871c8a5b6641d01e29bf5f53ef8ec688175e7a4
nuuco <nuuco.coding@gmail.com>
2022-10-03 01:22:23 +0900

    feat: roobit 스타일 선택 모달 구현

--- client/src/components/CreateRoobitModal.js
+++ client/src/components/CreateRoobitModal.js
@@ -1,33 +1,45 @@
 import { FormWrapper } from '../styled/Style';
 import Carousel from './Carousel';
+import RoobitStyleSelect from './RoobitStyleSelect';
 import { ReactComponent as CancelIcon } from '../images/cancel-icon.svg';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 const CreateRoobitModal = ({ handleOpenModal }) => {
-  const roobitTypes = useMemo(() => {
+  const [roobitType, setRoobitType] = useState('1');
+  const [roobitStyle, setRoobitStyle] = useState('A');
+  const [isPhaseOne, setIsPhaseOne] = useState(true);
+  const roobitTypesArr = useMemo(() => {
     return [
       {
         type: 'roobit',
         number: 1,
+        value: '1',
       },
       {
         type: 'roobit',
         number: 2,
+        value: '2',
       },
       {
         type: 'roobit',
-        number: 2,
+        number: 3,
+        value: '3',
       },
       {
         type: 'roobit',
-        number: 2,
+        number: 4,
+        value: '4',
       },
       {
         type: 'roobit',
-        number: 2,
+        number: 5,
+        value: '5',
       },
     ];
   }, []);
+  //axios 만들기
+  console.log(roobitType, roobitStyle);
+
   return (
     <FormWrapper
       width="476px"
@@ -37,13 +49,37 @@ const CreateRoobitModal = ({ handleOpenModal }) => {
       <CancelIcon stroke="#aaa" onClick={handleOpenModal} />
       <h2>Create a Roobits</h2>
       <form>
-        <section>
-          <p>고양이 선택</p>
-          <Carousel cards={roobitTypes} />
-        </section>
-        <section>
-          <p>글 작성하기</p>
-        </section>
+        {isPhaseOne ? (
+          <section>
+            <Carousel
+              cards={roobitTypesArr}
+              setData={setRoobitType}
+              roobitStyle={roobitStyle}
+            />
+            <RoobitStyleSelect setRoobitStyle={setRoobitStyle} />
+            <button onClick={() => setIsPhaseOne(false)}>
+              글 작성하러 가기
+            </button>
+          </section>
+        ) : (
+          <section>
+            <div>고양이</div>
+            <button onClick={() => setIsPhaseOne(true)}>
+              고양이 디자인 수정
+            </button>
+            <div>
+              <label htmlFor="from">from.</label>
+              <input id="from" />
+            </div>
+            <textarea />
+            <div>
+              <label htmlFor="to">to.</label>
+              <input id="to" />
+            </div>
+            <button type="reset">초기화</button>
+            <button type="button">작성 완료</button>
+          </section>
+        )}
       </form>
     </FormWrapper>
   );

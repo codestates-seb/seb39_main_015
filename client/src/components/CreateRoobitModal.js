eb3d198d343104e291afd7daec7a0cc51c67cce2
nuuco <nuuco.coding@gmail.com>
2022-10-03 03:52:37 +0900

    feat: 루빗 추가 모달 기본 구성

--- client/src/components/CreateRoobitModal.js
+++ client/src/components/CreateRoobitModal.js
@@ -3,6 +3,15 @@ import Carousel from './Carousel';
 import RoobitStyleSelect from './RoobitStyleSelect';
 import { ReactComponent as CancelIcon } from '../images/cancel-icon.svg';
 import { useMemo, useState } from 'react';
+import RoobitOneImg from '../styled/RoobitOneImg';
+import { getRoobitType } from '../hook/getRoobitType';
+import styled from 'styled-components';
+
+const RoobitImgWrapper = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+`;
 
 const CreateRoobitModal = ({ handleOpenModal }) => {
   const [roobitType, setRoobitType] = useState('1');
@@ -54,6 +63,7 @@ const CreateRoobitModal = ({ handleOpenModal }) => {
             <Carousel
               cards={roobitTypesArr}
               setData={setRoobitType}
+              roobitType={roobitType}
               roobitStyle={roobitStyle}
             />
             <RoobitStyleSelect setRoobitStyle={setRoobitStyle} />
@@ -63,21 +73,27 @@ const CreateRoobitModal = ({ handleOpenModal }) => {
           </section>
         ) : (
           <section>
-            <div>고양이</div>
+            <RoobitImgWrapper>
+              <RoobitOneImg
+                roobitCode={getRoobitType(roobitType + roobitStyle)}
+              />
+            </RoobitImgWrapper>
             <button onClick={() => setIsPhaseOne(true)}>
-              고양이 디자인 수정
+              루빗 디자인 수정
             </button>
             <div>
               <label htmlFor="from">from.</label>
-              <input id="from" />
+              <input id="from" placeholder="닉네임 (최대 10자)" required />
             </div>
-            <textarea />
+            <div>
+              <textarea placeholder="140자까지 작성 가능합니다." required />
+            </div>
             <div>
               <label htmlFor="to">to.</label>
-              <input id="to" />
+              <input id="to" defaultValue="everyone" required />
             </div>
             <button type="reset">초기화</button>
-            <button type="button">작성 완료</button>
+            <button type="submit">작성 완료</button>
           </section>
         )}
       </form>

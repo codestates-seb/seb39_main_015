f72719668f6b6c58a110a87d69814cb228ad2f59
CHOHYUNHWA <104138036+CHOHYUNHWA@users.noreply.github.com>
2022-10-02 13:33:40 +0900

    Feat/be/member (#157)
    * member frame create
    
    * feat:memberService logic create
    
    * feat: controller create
    
    * feat : Basic security create
    
    * fix: member Entity validation추가 정규표현식 미작성
    
    * fix member validation 적용
    
    * feat/fix : feat : 나의룸보기 구현, fix: deleteMember 기능 수정
    
    * feat: member 로그아웃 기능구현
    
    * feat : patchMember 기능 구현 외 회원가입,로그인,로그아웃,회원수정,회원삭제 테스트 완료
    
    * feat : 아이디찾기 기능구현
    
    * test: yml파일 삭제
    
    * test : gitignore 적용
    
    * feat: 나의룸 보기 기능
    
    * feat : 비밀번호 찾기기능 구현(이메일로 임시비밀번호발급)
    
    * fix :충돌 해결 2
    
    * fix : build를 위한 text부분 수정
    
    * fix : git ignore 적용
    
    * test: git ignore test
    
    * test : backend merge
    
    * test : backend branch merge
    
    * merge충돌
    
    * 충돌로 인해 reset
    
    * idea 삭제
    
    * initialize
    
    * fix branch
    
    * fix branch
    
    * fix branch
    
    * feat: 프론트 인증쪽 필요 인증 api, 아이디 중복체크 api 구현
    
    * feat: memberStatus 기능 추가 soft delete로 db관리
    
    * fix : cors관련 config수정
    
    * feat : jwt , OAuth2 구현
    
    * test : oauth test path open
    
    * test: oauth2 로그인 테스트
    
    * test : OAuth2 인증 테스트
    
    * feat : 홈페이지 회원가입 or Oauth2 회원가입인지 구분하여 user/auth 메서드 구현
    
    * feat : 이메일 중복체크api, 비밀번호검사 api 기능 구현
    
    * feat : 나의룸보기 기능 구현
    
    * fix: 아이디 찾기시 oauth로그인 사용자는 일치하는 회원정보가 없도록 수정
    
    * fix : myroom보기 기능에서 dday가 안불러와지는 오류 수정
    
    * feat : 이메일 인증 방식 구현 초안

--- server/src/main/java/seb15/roobits/mail/controller/MailController.java
+++ server/src/main/java/seb15/roobits/mail/controller/MailController.java
@@ -2,12 +2,15 @@ package seb15.roobits.mail.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import seb15.roobits.mail.dto.MailDto;
 import seb15.roobits.mail.service.SendMailService;
 import seb15.roobits.member.repository.MemberRepository;
 import seb15.roobits.member.service.MemberService;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,19 +24,18 @@ public class MailController {
     @Autowired
     private final SendMailService mailService;
 
-
-
-//    @GetMapping("/findpw")
-//    public Map<String,Boolean> pwFind(@RequestBody String email, String username){
-//        Map<String,Boolean> userCheck = new HashMap<>();
-//        boolean pwFindCheck = memberService.memberEmailCheck(email,username);
-//        userCheck.put("check",pwFindCheck);
-//        return userCheck;
-//    }
-
     @PostMapping("/findpw/sendemail")
-    public void sendEmail(@RequestBody MailDto mailDto){
-        MailDto sendMail = mailService.createMailAndChangePassword(mailDto);
-        mailService.mailSend(sendMail);
+    public void findPassword(@RequestBody MailDto.SendPassword mailDto){
+        MailDto.SendPassword sendMail = mailService.createMailAndChangePassword(mailDto);
+        mailService.findPasswordMailSend(sendMail);
+    }
+
+    @PostMapping("/auth/sendemail")
+    public ResponseEntity checkAuthEmail(@RequestBody MailDto.AuthEmail mailDto){
+        MailDto.AuthEmail senMail = mailService.createMailAndAuthEmail(mailDto);
+        mailService.authEmailMailSend(senMail);
+        MailDto.Key response = new MailDto.Key();
+        response.setCreateKey(senMail.getCreateKey());
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }

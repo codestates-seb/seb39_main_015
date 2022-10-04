09965926d313b5ca447d38c3b36fb616b3e37f01
nuuco <nuuco.coding@gmail.com>
2022-10-04 14:34:00 +0900

    feat: 플로팅 버튼 디자인 구현

--- client/src/pages/RoomDetail.js
+++ client/src/pages/RoomDetail.js
@@ -1,6 +1,8 @@
 import Building from '../components/Building';
 import CreateRoobitBtn from '../components/CreateRoobitBtn';
 import BackwardBtn from '../components/BackwardBtn';
+import LeftFloatingBtn from '../styled/LeftFloatingBtn';
+import styled from 'styled-components';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
@@ -13,8 +15,22 @@ import {
   roomDetailData_12,
 } from '../data/DummyData';
 import { getCookieValue } from '../hook/getCookieValue';
+import { useState } from 'react';
+
+/** 줌인 줌아웃 구현을 위한 styled-components */
+const Frame = styled.section`
+  &.zoom-in-mode {
+    transform: scale(2);
+  }
+
+  &.zoom-out-mode {
+    transform: scale(1);
+  }
+`;
 
 const RoomDetail = () => {
+  const [isZoomIn, setIsZoomIn] = useState(false);
+  setIsZoomIn;
   const { roomId } = useParams();
   const auth = getCookieValue('Authorization').length;
 
@@ -50,7 +66,15 @@ const RoomDetail = () => {
           <h1>{roomData.roomName}</h1>
           <p>{roomData.restDay}</p>
           <p>{roomData.dDay}</p>
-          <Building roobits={roobits} />
+          <Frame className={isZoomIn ? 'zoom-in-mode' : 'zoom-out-mode'}>
+            <Building roobits={roobits} />
+          </Frame>
+
+          <LeftFloatingBtn
+            className={isZoomIn ? 'zoom-out' : 'zoom-in'}
+            onClick={() => setIsZoomIn((prev) => !prev)}
+          />
+          <LeftFloatingBtn className="share" />
           <CreateRoobitBtn />
         </>
       )}

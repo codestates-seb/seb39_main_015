c14f6e0cbf0aef8a000500f41c3ece8798cde473
EstelleYU <iamestelleyu@gmail.com>
2022-10-04 09:08:24 +0900

    1004 YU #1 (명세서대로 response 수정 끝, nullDto 처리부터 수정하면 됨)

--- server/src/main/java/seb15/roobits/roobit/repository/RoobitRepository.java
+++ server/src/main/java/seb15/roobits/roobit/repository/RoobitRepository.java
@@ -7,24 +7,4 @@ import seb15.roobits.roobit.entity.Roobit;
 @Repository
 public interface RoobitRepository extends JpaRepository<Roobit, Long> {
 
-//    public static Page<Roobit> findByRoomId(long roomId, PageRequest roobitId){
-//        return null;
-//    }
-//
-//    public Roobit findRoobitByRoomId(long roomId);
-
-//    @EntityGraph(attributePaths = {"roobits"})
-//    List<Roobits> findAll();
-
-//    @Query(value = "SELECT c FROM Roobit c WHERE c.roomId = :roomId")
-//    Page<Roobit> findByRoomId(long roomId);
-
-//    static Page<Roobit> findByRoomId(long roomId, Pageable pageable) {
-//        return null;
-//    }
-
-
-    // 0929 Create @Query Find Instance Method 찾아보기 (JPA pallete)
-    // 위 repository 작성법 찾아보기
-
 }

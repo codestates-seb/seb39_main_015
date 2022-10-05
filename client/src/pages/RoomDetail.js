143de27f6334c2bd86667d23914e865bf00d9219
nuuco <nuuco.coding@gmail.com>
2022-10-05 09:10:44 +0900

    feat: 로딩 컴포넌트 추가, api 주소 원 상태

--- client/src/pages/RoomDetail.js
+++ client/src/pages/RoomDetail.js
@@ -2,16 +2,17 @@ import Building from '../components/Building';
 import CreateRoobitBtn from '../components/CreateRoobitBtn';
 import BackwardBtn from '../components/BackwardBtn';
 import LeftFloatingBtn from '../styled/LeftFloatingBtn';
+import { Loading } from '../components/Loading';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import {
-  roomDetailData,
+  //roomDetailData,
   //roomDetailData_1,
   //roomDetailData_2,
   //roomDetailData_3,
   //roomDetailData_4,
-  //roomDetailData_12,
+  roomDetailData_16,
 } from '../data/DummyData';
 import { getCookieValue } from '../hook/getCookieValue';
 import { useState } from 'react';
@@ -25,13 +26,12 @@ const RoomDetail = () => {
   const auth = getCookieValue('Authorization').length;
   roomId;
 
-  //`${process.env.REACT_APP_API_URL}/rooms/${roomId}`
   const { data, isLoading } = useQuery(
     'roobits',
     () =>
       axios
-        .get(`/fake`)
-        .catch(() => roomDetailData /* 실패할 경우 더미 데이터 표시 */),
+        .get(`${process.env.REACT_APP_API_URL}/rooms/${roomId}`)
+        .catch(() => roomDetailData_16 /* 실패할 경우 더미 데이터 표시 */),
     {
       staleTime: 1000 * 60 * 30,
       retry: 1,
@@ -45,7 +45,7 @@ const RoomDetail = () => {
   if (!isLoading) {
     ({ roomStatus, roomData, roobits } = data);
   } else {
-    return <p>로딩 컴포넌트</p>;
+    return <Loading />;
   }
 
   return (

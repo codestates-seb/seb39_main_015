2220ce3257447203115b81da7486f886ed8b26c1
EstelleYU <iamestelleyu@gmail.com>
2022-10-05 16:52:27 +0900

    1005 #6 YU ( body Null 주석 처리 )

--- server/src/main/java/seb15/roobits/roobit/dto/RoobitNullResponseDto.java
+++ server/src/main/java/seb15/roobits/roobit/dto/RoobitNullResponseDto.java
@@ -25,7 +25,7 @@ public class RoobitNullResponseDto {  // 디데이 아닐 땐 body는 Null
     }
 
     private String nickname;
-    private String body;
+//    private String body;
     private String email;
     private String reception;
 //    private String ToReception;  // 1005YU
